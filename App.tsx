975f9ca15e8c0c3738b5a27b419ecf3dbe3ea14b
quoclong20222428 <4801104085@student.hcmue.edu.vn>
2024-09-12 20:37:57 +0700

    Caculator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import { useState } from 'react';
+import Caculator from './Caculator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style ={{flex: 1}}>
+      <Caculator />
+    </SafeAreaView>
   );
 }
 
